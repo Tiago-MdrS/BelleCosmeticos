cea4c2b24a5dbe0f5f9125043427a85a02e67dce
Tiago-MdrS <https://github.com/Tiago-MdrS>
2026-05-04 00:20:09 -0300

    dependencias faltando

--- electron/main.js
+++ electron/main.js
@@ -1,34 +1,77 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
+const { spawn } = require('child_process');
+const waitOn = require('wait-on');
 
 let mainWindow;
+let backendProcess;
 
-function createWindow() {
-  mainWindow = new BrowserWindow({
-    width: 1400,
-    height: 900,
-    minWidth: 1024,
-    minHeight: 768,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-      contextIsolation: true
-    },
-    icon: path.join(__dirname, '../assets/loja.ico')
-  });
+const isDev = !app.isPackaged;
 
-  if (!app.isPackaged) {
-    mainWindow.loadURL('http://localhost:5173');
-  } else {
-    mainWindow.loadFile(path.join(__dirname, '../dist/index.html'));
-  }
+function startBackend() {
+  const backendPath = isDev
+    ? path.join(__dirname, '../backend/src/server.js')
+    : path.join(process.resourcesPath, 'backend/src/server.js');
 
-  mainWindow.on('closed', () => {
-    mainWindow = null;
+  backendProcess = spawn('node', [backendPath], {
+    cwd: isDev
+      ? path.join(__dirname, '../backend')
+      : path.join(process.resourcesPath, 'backend'),
+    shell: true,
+    stdio: 'inherit',
   });
 }
 
-app.whenReady().then(createWindow);
+async function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    minWidth: 1100,
+    minHeight: 700,
+    icon: path.join(__dirname, '../assets/loja.ico'),
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+    },
+  });
+
+  if (isDev) {
+    await waitOn({
+      resources: ['http://localhost:3333/produtos'],
+      timeout: 15000,
+    });
+
+    mainWindow.loadURL('http://localhost:5173');
+  } else {
+    await waitOn({
+      resources: ['http://localhost:3333/produtos'],
+      timeout: 15000,
+    });
+
+    mainWindow.loadFile(path.join(__dirname, '../dist/index.html'));
+  }
+}
+
+app.whenReady().then(async () => {
+  startBackend();
+
+  setTimeout(() => {
+    createWindow();
+  }, 1000);
+});
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') app.quit();
+  if (backendProcess) {
+    backendProcess.kill();
+  }
+
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('before-quit', () => {
+  if (backendProcess) {
+    backendProcess.kill();
+  }
 });
